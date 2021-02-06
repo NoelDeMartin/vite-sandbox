49f2652dfabf4305db87c140e24e9c1488f61625
Noel De Martin <noeldemartin@gmail.com>
2021-02-06 13:44:36 +0100

    set global Stream

--- main.js
+++ main.js
@@ -1,11 +1,15 @@
 window.global = window;
 
-import('jsonld-streaming-parser').then(({ JsonLdParser }) => {
-    const parser = new JsonLdParser();
+import('stream').then(({ Stream}) => {
+    window.Stream = Stream;
 
-    parser.on('data', quad => console.log('quad', quad))
-    parser.on('error', error => console.error(error))
-    parser.on('end', () => console.log('done!'))
-    parser.write('{"@context":"https://schema.org","@type":"Movie","name":"Foobar"}');
-    parser.end();
+    import('jsonld-streaming-parser').then(({ JsonLdParser }) => {
+        const parser = new JsonLdParser();
+
+        parser.on('data', quad => console.log('quad', quad))
+        parser.on('error', error => console.error(error))
+        parser.on('end', () => console.log('done!'))
+        parser.write('{"@context":"https://schema.org","@type":"Movie","name":"Foobar"}');
+        parser.end();
+    })
 })
